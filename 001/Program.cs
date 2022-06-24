86c67b0f82191b0864d922457d8110aaa6db3719
litovkomilada <litovkomiada@yandex.ru>
2022-06-24 19:43:12 +0300

    Example 001

---
+++ 001/Program.cs
@@ -0,0 +1,7 @@
+﻿//int number = 5;
+Console.WriteLine("Введите число, которое нужно возвести в квадрат");
+int number = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(number*number);
+//Console.WriteLine(Math.Pow(number, 2));
+//Console.WriteLine("5 в квадрате равно " + Math.Pow(number, 2));
+Console.WriteLine($"{number} в квадрате равно {Math.Pow(number, 2)}");
