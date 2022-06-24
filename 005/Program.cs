ad7a446187007e3862431ae22a5ad9ce3c5b3ca5
litovkomilada <litovkomiada@yandex.ru>
2022-06-24 20:38:43 +0300

    Example 005

---
+++ 005/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = -number;
+
+if(number>0)
+{
+    while(count<=number)
+    {
+        Console.Write(count + " ");
+        count++;
+    }
+}
+
+else
+{
+     while(number<=count)
+    {
+        Console.Write(number + " ");
+        number++;
+    }
+}
