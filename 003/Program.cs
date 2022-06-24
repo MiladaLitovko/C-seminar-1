882ce6c71adb6c056f64a169af9789b87969eca4
litovkomilada <litovkomiada@yandex.ru>
2022-06-24 20:11:05 +0300

    Example 003

---
+++ 003/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите номер, соответствующий дню недели");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a == 1) Console.Write ("понедельник");
+else if (a == 2) Console.Write ("вторник");
+else if (a == 3) Console.Write ("среда");
+else if (a == 4) Console.Write ("четверг");
+else if (a == 5) Console.Write ("пятница");
+else if (a == 6) Console.Write ("суббота");
+else if (a == 7) Console.Write ("воскресенье");
