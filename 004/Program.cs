dfd94fba81aa21b2cc066a2f8f08b119966223df
litovkomilada <litovkomiada@yandex.ru>
2022-06-24 20:27:23 +0300

    Example 004

---
+++ 004/Program.cs
@@ -0,0 +1,12 @@
+﻿//функция(процедура) возведения числа в квадрат
+
+int number = 6;
+OurPower(number); // вызов метода
+OurPower(7); // вызов метода
+OurPower(number + number); // вызов метода
+
+void OurPower(int currentnumber)
+{
+    Console.WriteLine(currentnumber * currentnumber);
+}
+
